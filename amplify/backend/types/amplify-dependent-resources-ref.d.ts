c850a320d13686fa00b6805339d1a5c161e5ad6f
ohisama <second.2053.tjpamw@outlook.jp>
2022-10-20 13:29:31 +0900

    ver1.0

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "tsgql": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
